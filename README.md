cb1ae023d11a8c667953b040ee65783a6439bdd8
Lucas Rada <l.rada@alumno.etec.um.edu.ar>
2024-06-28 12:49:41 -0300

    Updates README, adds JSDoc syntax

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-Lucas Rada
+# Preparación
+Primero hay que instalar los modulos de Node.js.
+
+    npm install
+
+Una vez instalados, se puede correr el projecto.
+
+    npm run dev
+
+# Uso
+#### Navegador
+El servidor corre de forma local en el puerto 3000.
+```
+127.0.0.1:3000
+```
+En la dirección **/books** podemos ver el listado de libros.
+```
+127.0.0.1:3000/books
+```
+Al ingresar el *id* después de la dirección /books es posible ver un libro en especifico.
+```
+127.0.0.1:3000/books/1
+```
+#### requests.http
+Usando el archivo **requests.http** se puede ejecutar un comando **GET** para obtener el listado de libros.
+```http
+GET http://localhost:3000/books HTTP/1.1
+```
+También podemos pedir especificamente un libro.
+```http
+GET http://localhost:3000/books/1 HTTP/1.1
+```
+Para añadir un libro, hay un comando **POST** de demostración.
+```http
+POST http://localhost:3000/books HTTP/1.1
+Content-Type:  application/json
+
+{
+"autor":  "J. K. Rowling",
+"isbn":  "9789878000107",
+"editorial":  "Bloomsbury Salamandra Scholastic",
+"paginas":  "256"
+}
+```
+Y, luego, utilizando el comando **PUT** se puede actualizar la información de un libro con un *id* especifico.
+```http
+PUT http://localhost:3000/books/1 HTTP/1.1
+Content-Type:  application/json
+
+{
+"autor":  "J. K. Rowling",
+"isbn":  "9789878000107",
+"editorial":  "Bloomsbury Salamandra Scholastic",
+"paginas":  "256"
+}
+```
+##### Autor: Lucas Rada
--- index.js
+++ index.js
@@ -7,18 +7,47 @@ const app = express();
 const port = 3000;
 
 config();
-const filename = 'database.db'
-console.log(filename)
+const filename = 'database.db';
+console.log(filename);
+
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: filename
 });
 
-class Book extends Model { }
+/**
+ * Represents a Book.
+ * @class
+ * @extends Model
+ */
+class Book extends Model {}
+
+/**
+ * Initializes the Book model.
+ */
 Book.init({
+    /**
+     * The author of the book.
+     * @type {string}
+     */
     autor: DataTypes.STRING,
+
+    /**
+     * The ISBN of the book.
+     * @type {number}
+     */
     isbn: DataTypes.INTEGER,
+
+    /**
+     * The editorial of the book.
+     * @type {string}
+     */
     editorial: DataTypes.STRING,
+
+    /**
+     * The number of pages in the book.
+     * @type {number}
+     */
     paginas: DataTypes.INTEGER
 }, { sequelize, modelName: 'book' });
 
@@ -27,21 +56,61 @@ sequelize.sync();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/**
+ * GET /books
+ * Retrieves all books.
+ * @name get/books
+ * @function
+ * @memberof module:routes/books
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 app.get('/books', async (req, res) => {
     const books = await Book.findAll();
     res.json(books);
 });
 
+/**
+ * GET /books/:id
+ * Retrieves a book by its ID.
+ * @name get/books/:id
+ * @function
+ * @memberof module:routes/books
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 app.get('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     res.json(book);
 });
 
+/**
+ * POST /books
+ * Creates a new book.
+ * @name post/books
+ * @function
+ * @memberof module:routes/books
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 app.post('/books', async (req, res) => {
     const book = await Book.create(req.body);
     res.json(book);
 });
 
+/**
+ * PUT /books/:id
+ * Updates a book by its ID.
+ * @name put/books/:id
+ * @function
+ * @memberof module:routes/books
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 app.put('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -52,6 +121,16 @@ app.put('/books/:id', async (req, res) => {
     }
 });
 
+/**
+ * DELETE /books/:id
+ * Deletes a book by its ID.
+ * @name delete/books/:id
+ * @function
+ * @memberof module:routes/books
+ * @inner
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 app.delete('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -62,6 +141,15 @@ app.delete('/books/:id', async (req, res) => {
     }
 });
 
+/**
+ * Starts the server.
+ * @name listen
+ * @function
+ * @memberof module:server
+ * @inner
+ * @param {number} port - Port number for the server to listen on
+ * @param {Function} callback - Callback function to execute once the server starts
+ */
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`);
 });
